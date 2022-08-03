bfdc0dbba171c8327d7574941745582445fe383c
ohtjqkd <ohtjqkd@gmail.com>
2022-08-04 00:01:30 +0900

    first example finished

--- 1. Blockchain Intuition/blockchain.py
+++ 1. Blockchain Intuition/blockchain.py
@@ -3,7 +3,7 @@
 import datetime
 import hashlib
 import json
-# from flask import Flask
+from flask import Flask, jsonify
 
 # Part 1 - Building a Blockchain
 
@@ -56,3 +56,44 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+# Part2 - Mining our Blockchain
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations, you just mined a block!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    response = {
+        'valid': blockchain.is_chain_valid(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+# Running the app
+app.run(host='0.0.0.0', port=5000)
