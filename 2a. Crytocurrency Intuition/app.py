d05e3406d333cf1c273a30df9acb1f10eb869771
ohtjqkd <ohtjqkd@gmail.com>
2022-10-20 01:17:12 +0900

    add Cryptocurrency/app.py

---
+++ 2a. Crytocurrency Intuition/app.py
@@ -0,0 +1,134 @@
+# Module 1 - Create a blockchain
+
+import datetime
+import hashlib
+import json
+from xmlrpc.client import Boolean
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash,
+            'transactions': self.transactions
+        }
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]  # genesis block이 있다는 전제임
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            # hash 연산은 개발자 정의, 비대칭성이라는 것은 인자의 순서가 바뀐 경우에 다른 값이 나올 수 있게?
+            # ex) new_proof + previous_proof = previous_proof + new_proof 이기 때문에 비대칭성을 가질 수 있는 연산으로 바꾸어야함
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation.startswith('0000'):
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if [block['previous_hash'] != self.hash(previous_block)]:
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if not hash_operation.startswith('0000'):
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+
+    def add_transactions(self, sender, receiver, amount):
+        self.transactions.append({
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount
+        })
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self) -> Boolean:
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'{node}/get_chain')
+            if response.status_code == 200 and response.json()['length'] > max_length and self.is_chain_valid(response.json()['chain']):
+                longest_chain = response
+        if longest_chain:
+            self.chain = longest_chain['chain']
+            return True
+        return False
+
+# Part2 - Mining our Blockchain
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations, you just mined a block!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    response = {
+        'valid': blockchain.is_chain_valid(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+# Running the app
+app.run(host='0.0.0.0', port=5000)
